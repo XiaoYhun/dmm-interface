cbba6b6d55389474031022ed2d3a9da93ecfa37a
Hoang Trinh <hoang.trinhj@gmail.com>
2021-12-12 10:45:26 +0700

    feat: Integrate Aurora Network

--- src/components/NetworkModal/index.tsx
+++ src/components/NetworkModal/index.tsx
@@ -93,36 +93,42 @@ export default function NetworkModal(): JSX.Element | null {
       <ModalHeader title={t`Select a Network`} />
 
       <NetworkList>
-        {[ChainId.MAINNET, ChainId.MATIC, ChainId.BSCMAINNET, ChainId.AVAXMAINNET, ChainId.FANTOM, ChainId.CRONOS].map(
-          (key: ChainId, i: number) => {
-            if (chainId === key) {
-              return (
-                <SelectNetworkButton key={i} padding="0">
-                  <ListItem selected>
-                    <img src={NETWORK_ICON[key]} alt="Switch Network" style={{ width: '24px', marginRight: '8px' }} />
-                    <NetworkLabel>{NETWORK_LABEL[key]}</NetworkLabel>
-                  </ListItem>
-                </SelectNetworkButton>
-              )
-            }
-
+        {[
+          ChainId.MAINNET,
+          ChainId.MATIC,
+          ChainId.BSCMAINNET,
+          ChainId.AVAXMAINNET,
+          ChainId.FANTOM,
+          ChainId.CRONOS,
+          ChainId.AURORA
+        ].map((key: ChainId, i: number) => {
+          if (chainId === key) {
             return (
-              <SelectNetworkButton
-                key={i}
-                padding="0"
-                onClick={() => {
-                  toggleNetworkModal()
-                  changeNetwork(key)
-                }}
-              >
-                <ListItem>
+              <SelectNetworkButton key={i} padding="0">
+                <ListItem selected>
                   <img src={NETWORK_ICON[key]} alt="Switch Network" style={{ width: '24px', marginRight: '8px' }} />
                   <NetworkLabel>{NETWORK_LABEL[key]}</NetworkLabel>
                 </ListItem>
               </SelectNetworkButton>
             )
           }
-        )}
+
+          return (
+            <SelectNetworkButton
+              key={i}
+              padding="0"
+              onClick={() => {
+                toggleNetworkModal()
+                changeNetwork(key)
+              }}
+            >
+              <ListItem>
+                <img src={NETWORK_ICON[key]} alt="Switch Network" style={{ width: '24px', marginRight: '8px' }} />
+                <NetworkLabel>{NETWORK_LABEL[key]}</NetworkLabel>
+              </ListItem>
+            </SelectNetworkButton>
+          )
+        })}
       </NetworkList>
     </ModalContentWrapper>
   )
